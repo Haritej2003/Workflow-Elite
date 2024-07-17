9a58a1348ceb5420180a9f54fe53e702fdf444b1
Haritej2003 <harijuvvagani@gmail.com>
2024-07-17 11:01:45 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const connectMongodb = require("./init/mongodb");
+const todoRoute = require("./routes/todo");
+const dotenv = require("dotenv");
+
+// environment variable
+dotenv.config();
+const app = express();
+
+connectMongodb();
+
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", todoRoute);
+
+module.exports = app;
